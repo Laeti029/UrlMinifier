7afc8c23484b4a052c6c62263e56de187d8b32a6
Damien Wido <ornixx44@gmail.com>
2017-03-20 16:20:36 +0100

    Architecture TU

--- src/tests/indexSpec.js
+++ src/tests/indexSpec.js
@@ -1,8 +1,8 @@
-var calculator = require("../controllers/indexCtrl");
+let indexCtrl = require("../controllers/indexController");
 
 describe("multiplication", function () {
   it("should multiply 2 and 3", function () {
-    var product = calculator.multiply(2, 3);
+    var product = indexCtrl.multiply(2, 3);
     expect(product).toBe(15);
   });
-});    
+});
