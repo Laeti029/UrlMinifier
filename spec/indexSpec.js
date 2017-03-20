4f892be4b5417030732772b8732db7415ef17dd2
Jérémy Lejeune <lejeune.jeremy@laposte.net>
2017-03-20 10:14:49 +0100

    Added Jasmine

--- spec/indexSpec.js
+++ spec/indexSpec.js
@@ -1,1 +1,21 @@
-plop
+const index = require('../dev/Index');
+
+describe("Index", () => {
+    describe("=> isNoGood", () => {
+        it("Should return false", next => {
+            let result = index();
+
+            expect(result).toBeFalsy();
+
+            next();
+        });
+
+        it("Should return true", next => {
+            let result = index();
+
+            expect(result).toBeTruthy();
+
+            next();
+        });
+    });
+});
