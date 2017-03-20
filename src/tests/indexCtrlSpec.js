1ce3398fff6c7affc382c727bf38aa12942c7476
Jérémy Lejeune <lejeune.jeremy@laposte.net>
2017-03-20 12:01:42 +0100

    Added controllers & models folders

---
+++ src/tests/indexCtrlSpec.js
@@ -0,0 +1,9 @@
+const IndexCtrl = require('./src/controllers/indexCtrl');
+
+describe('IndexCtrl', () => {
+  describe('index', () => {
+    it('Should return idex views', () => {
+
+    });
+  });
+});
