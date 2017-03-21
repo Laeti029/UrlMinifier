f6906ac9bf71e1fd3d6fa1e34b2f6af575fd2d3a
Whispmoon <tbilliau@gmail.com>
2017-03-21 11:10:11 +0100

    url entity/route/controller

---
+++ src/controllers/urlController.js
@@ -0,0 +1,15 @@
+const UrlService = require('../services/UrlService');
+class UrlController {
+    addUrl(req, res) {
+        let post = req.body;
+        let tmp = post.url;
+        console.log(tmp);
+    }
+    removeUrl(req, res) {
+        res.send('remove Url')
+    }
+    getUrls(req, res) {
+        res.send('get urls')
+    }
+}
+module.exports = new UrlController();
