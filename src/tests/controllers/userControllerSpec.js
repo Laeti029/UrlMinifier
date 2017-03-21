35487c331dbda204d0a3c1b435437929b41be3a0
PC-PAUL\paulm <paul.morice.pro@gmail.com>
2017-03-21 11:47:39 +0100

    Add unserControllerSpec

---
+++ src/tests/controllers/userControllerSpec.js
@@ -0,0 +1,43 @@
+let userValidator = require("../services/userController");
+
+/* Fonction register */
+describe("register", function () {
+  it("Test de la fonction register - cas nominal", function () {
+    let register = registerAction();
+    let result = register.length > 20;
+    expect(result).toBe(true);
+  });
+
+  it("Test de la fonction register - email manquant", function () {
+    let register = registerAction();
+    let result = register.length > 20;
+    expect(result).toBe(false);
+  });
+
+  it("Test de la fonction register - password manquant", function () {
+    let register = registerAction();
+    let result = register.length > 20;
+    expect(result).toBe(false);
+  });
+});
+
+/* Fonction connect */
+describe("connect", function () {
+  it("Test de la fonction register - cas nominal", function () {
+    let connect = connectAction();
+    let result = connect.length > 20;
+    expect(result).toBe(true);
+  });
+
+  it("Test de la fonction register - email manquant", function () {
+    let connect = connectAction();
+    let result = connect.length > 20;
+    expect(result).toBe(false);
+  });
+
+  it("Test de la fonction register - password manquant", function () {
+    let connect = connectAction();
+    let result = connect.length > 20;
+    expect(result).toBe(false);
+  });
+});
