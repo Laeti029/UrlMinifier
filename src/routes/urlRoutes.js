1a4ace8ddad01adcbb6150d3f492fc9366a00ae5
Damien Wido <ornixx44@gmail.com>
2017-03-21 11:26:28 +0100

    Routes URL

--- src/routes/urlRoutes.js
+++ src/routes/urlRoutes.js
@@ -1,9 +1,10 @@
-const express = require('express');
 const router = express.Router();
 const UrlController = require('../controllers/UrlController');
-const bodyParser = require('body-parser');
-var jsonParser = bodyParser.json();
-router.post('/add-url', jsonParser, UrlController.addUrl);
-router.post('/remove-url', UrlController.removeUrl);
-router.post('/get-urls', UrlController.getUrls);
+let jsonParser = bodyParser.json();
+
+router.post('/url', jsonParser, UrlController.addUrl);
+router.delete('/url', UrlController.removeUrl);
+router.get('/url', UrlController.getUrls);
+
+
 module.exports = router;
