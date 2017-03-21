a1d495670130955f564a0460301cdcfb115c8e29
Clem <imie@localhost.localdomain>
2017-03-21 10:18:19 +0100

    add mongodb connection

--- src/server.js
+++ src/server.js
@@ -4,8 +4,20 @@ const express = require("express");
 
 import { Server } from 'http';
 
+const MongoClient = require('mongodb').MongoClient
+
 let app = express();
 let http = Server(app);
+let db;
+
+MongoClient.connect('mongodb://root:Password1234@ds137370.mlab.com:37370/urlminifier', (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  app.listen(3000, () => {
+    console.log('listening on 3000')
+  })
+})
+
 
 app.use(express.static(__dirname + '/public'));
 
