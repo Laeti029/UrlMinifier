fcf9bd6d56fe1e4c90f4ab693ff8d26db953bf3a
Clem <imie@localhost.localdomain>
2017-03-21 16:20:52 +0100

    add emailValidator ans passwordValidator

--- src/services/userValidator.js
+++ src/services/userValidator.js
@@ -1,26 +1,26 @@
 module.exports = {
+
   emailValidator: function (email) {
-    if (email != "") {
+    if (email.match(/^[a-z0-9_.+-]+@[a-z0-9-]+\.[a-z0-9-.]+$/i)) {
+   	  return true;
+   	} else {
+   	  return false
+   	}
+  },
+
+  passwordValidator: function(password) {
+    if (password.match(/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@!?#]).{5,}/)) {
       return true;
     } else {
       return false;
     }
-  },
-  passwordValidator(password) {
-    if (password === "1233456") {
-      return true;
-    } else {
-      return false;
-    }
-  },
-  register(email, password) {
-    if (passwordValidator(password) && emailValidator(password)){
-      return true;
-    } else {
-      return false;
-    }
-  },
-  login(email, password) {
-    
+  }
+
+  register: function(email, password) {
+  	// insert new user in mongodb
+  }
+
+  login: function(email, password) {
+  	// search for user in mongodb by email
   }
 };
