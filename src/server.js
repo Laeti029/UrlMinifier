88de0af84d99ca8a108bab2a8109b80bb1d64c53
WoufWouf <ludovic.garraud@gmail.com>
2017-03-22 15:05:46 +0100

    setup cors middleware (quick and dirty)

--- src/server.js
+++ src/server.js
@@ -19,9 +19,20 @@ mongoose.connect('mongodb://root:Password1234@ds137370.mlab.com:37370/urlminifie
     console.log('MongoDB listening on port 3000')
   })
 })
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", req.get('Origin'));
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Origin");
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Max-Age", 3600);
+
+    if(req.method === 'OPTIONS')
+        res.sendStatus(200)
+    else
+        next();
+})
 app.use(userRouter);
 app.use(urlRouter);
 
